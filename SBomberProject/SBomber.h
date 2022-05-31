026c375837508f002ff403338b84e056cde50d43
Kate <c.chistova2014@yandex.ru>
2022-05-31 17:07:41 +0300

    Добавьте файлы проекта.

---
+++ SBomberProject/SBomber.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+
+
+class SBomberImpl;
+
+class SBomber
+{
+public:
+
+    SBomber();
+    ~SBomber();
+
+    bool GetExitFlag() const;
+
+    void ProcessKBHit();
+    void TimeStart();
+    void TimeFinish();
+
+    void DrawFrame();
+    void MoveObjects();
+    void CheckObjects();
+
+private:
+
+    std::unique_ptr<SBomberImpl> impl;
+
+};
